dc169666f38f3cf264d96162589ac9bea8a8907d
Sowmiya Nagarajan <sowmiya_n@intuit.com>
2020-03-12 22:47:17 +0530

    Adding UI module

--- stack_box.sh
+++ stack_box.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+echo "     _             _    _"
+echo " ___| |_ __ _  ___| | _| |__   _____  __"
+echo "/ __| __/ _  |/ __| |/ / '_ \ / _ \ \/ /"
+echo "\__ \ || (_| | (__|   <| |_) | (_) >  <"
+echo '|___/\__\__,_|\___|_|\_\_.__/ \___/_/\_\'
+echo "\n"
+
 args="$*"
 python stack.py $args
 
@@ -8,13 +15,6 @@ beginswith() { case $2 in "$1"*) true;; *) false;; esac; }
 docker-compose down > logs/docker-compose-down-log.txt
 docker-compose build > logs/docker-compose-build-log.txt
 docker-compose up -d --remove-orphans
-
-echo "     _             _    _"
-echo " ___| |_ __ _  ___| | _| |__   _____  __"
-echo "/ __| __/ _  |/ __| |/ / '_ \ / _ \ \/ /"
-echo "\__ \ || (_| | (__|   <| |_) | (_) >  <"
-echo '|___/\__\__,_|\___|_|\_\_.__/ \___/_/\_\'
-echo "\n"
 
 containers=$(docker ps --format '{{.Names}}')
 ports="$(docker ps --format '{{.Ports}}')"
