399d428a5f19d5bd68506e2bfc9256eb99e85d05
Denys Lins <denys.lins@hp.com>
2020-04-24 20:43:13 -0300

    Add eslint

---
+++ react/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  parser: "babel-eslint",
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      impliedStrict: true,
+      jsx: true,
+    },
+    sourceType: "module",
+  },
+  plugins: ["react", "react-hooks"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {},
+};
