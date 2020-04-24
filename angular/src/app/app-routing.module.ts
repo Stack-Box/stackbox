3ac7747e2e520c33ad940e208e58e1c6e9f2b652
Sowmiya Nagarajan <sowmiyanagarajan98@gmail.com>
2020-04-24 23:33:13 +0530

    Fixes 54 and 55

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MysqlComponent} from "./mysql/mysql.component";
+import {ElasticsearchComponent} from "./elasticsearch/elasticsearch.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'mysql', component: MysqlComponent },
+  { path: 'elasticsearch', component: ElasticsearchComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
